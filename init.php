46a84b4c1df0cf5b18e90bbd73cc4a25fc67cd51
JuanAndresAyala <Juanandresa170@gmail.com>
2024-08-06 15:31:05 -0500

    primer commit

--- init.php
+++ init.php
@@ -11,3 +11,5 @@ require( 'inc/publicaciones.php');
 require ('inc/class-db.php');
 
 $app_db = new DB(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+require (__DIR__ . '/inc/helpers.php'); 
